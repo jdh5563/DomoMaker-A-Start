97d87438a1adf165f1bcf445e847a36c3bacb4ce
John Heiden <jdh5563@rit.edu>
2022-03-31 00:20:29 -0400

    Resolved error where landing page would infinitely redirect

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -14,13 +14,13 @@ const requiresLogout = (req, res, next) => {
   return next();
 };
 
-// const requiresSecure = (req, res, next) => {
-//   if (!req.headers['x-forwarded-proto'] !== 'https') {
-//     return res.redirect(`https://${req.hostname}${req.url}`);
-//   }
+const requiresSecure = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(`https://${req.hostname}${req.url}`);
+  }
 
-//   return next();
-// };
+  return next();
+};
 
 const bypassSecure = (req, res, next) => {
   next();
@@ -29,5 +29,5 @@ const bypassSecure = (req, res, next) => {
 module.exports.requiresLogin = requiresLogin;
 module.exports.requiresLogout = requiresLogout;
 
-if (process.env.NODE_ENV === 'production') module.exports.requiresSecure = bypassSecure;
+if (process.env.NODE_ENV === 'production') module.exports.requiresSecure = requiresSecure;
 else module.exports.requiresSecure = bypassSecure;
